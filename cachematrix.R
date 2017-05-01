b46b7c44b4ba7077d6cdb2a05354baffc5025679
DataScientologist <joaquin.mccoy@gmail.com>
2017-04-30 22:02:39 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## References:
+## https://www.mathsisfun.com/algebra/matrix-inverse.html
+## https://www.mathsisfun.com/algebra/matrix-introduction.html
+
+## Creates a cached matrix for later evalution.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Checks if mean exists or gets the mean
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
